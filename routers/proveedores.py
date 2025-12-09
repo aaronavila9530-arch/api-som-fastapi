4394edee737a7e6c09b0e619c08b6e40a493d68a
Aaron Avila <aaronavila9530@gmail.com>
2025-12-09 14:28:50 -0600

    actualizacion

--- routers/proveedores.py
+++ routers/proveedores.py
@@ -4,27 +4,12 @@ import database
 router = APIRouter(prefix="/proveedores", tags=["Proveedores"])
 
 
-@router.post("/add")
-def add_proveedor(data: dict):
+@router.get("/ultimo_codigo")
+def ultimo_codigo():
     try:
-        sql = """
-        INSERT INTO proveedor (
-            codigo, nombre, apellidos, nombrecomercial, cedula_vat,
-            pais, provincia, canton, distrito, direccionexacta,
-            prefijo, telefono, correo, terminospago, banco,
-            cuenta_iban, swiftcode, uid, direccionbanco,
-            tipoproveeduria, comentarios
-        )
-        VALUES (
-            %(Codigo)s, %(Nombre)s, %(Apellidos)s, %(NombreComercial)s, %(Cedula)s,
-            %(Pais)s, %(Provincia)s, %(Canton)s, %(Distrito)s, %(DireccionExacta)s,
-            %(Prefijo)s, %(Telefono)s, %(Correo)s, %(TerminosPago)s, %(Banco)s,
-            %(CuentaIBAN)s, %(SwiftCode)s, %(UID)s, %(DireccionBanco)s,
-            %(TipoProveeduria)s, %(Comentarios)s
-        );
-        """
-        database.sql(sql, data)
-        return {"status": "OK", "msg": "Proveedor registrado 💾✔"}
-
+        res = database.sql("SELECT codigo FROM proveedor ORDER BY id DESC LIMIT 1;", fetch=True)
+        if res:
+            return {"codigo": res[0]["codigo"]}
+        return {"codigo": None}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))

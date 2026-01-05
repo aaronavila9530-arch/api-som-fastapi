dda7ed6d67f1f8757f1b965a1e32a84932e7d4a5
Aaron Avila <aaronavila9530@gmail.com>
2026-01-04 20:56:04 -0600

    invoicing

--- routers/factura.py
+++ routers/factura.py
@@ -10,7 +10,7 @@ from fastapi import (
 )
 
 from psycopg2.extras import RealDictCursor
-from datetime import datetime
+from datetime import datetime, date
 from fastapi.responses import FileResponse
 import os
 import uuid
